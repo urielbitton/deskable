c23729c34290db1fffd9bf1654aa5d3ccfc4dd6c
urielbitton <urielas@hotmail.com>
2023-11-09 14:14:55 -0500

    tinymce lineheight fixes

--- src/app/components/ui/PageEditor.js
+++ src/app/components/ui/PageEditor.js
@@ -18,6 +18,7 @@ export default function PageEditor(props) {
       onFocus={onFocus}
       init={{
         height: editorHeight,
+        lineheight_formats: "0.5 0.75 1 1.25 1.5 2 3 4 5 6", 
         placeholder: 'Start jotting down ideas here...or use the sidebar templates to get inspired quickly.',
         menubar: false,
         statusbar: false,
@@ -36,16 +37,16 @@ export default function PageEditor(props) {
           'anchor', 'searchreplace', 'fullscreen', 'emoticons',
           'insertdatetime', 'media', 'table', 'help', 'wordcount', 'inlinecode',
           'codesample', 'template', 'fontSelect', 'fontsizeselect',
-          'print', 'insertdatetime', 'pagebreak', 'code',
+          'print', 'insertdatetime', 'pagebreak', 'code', 'lineheight', 
         ],
         toolbar: 'customBtn undo redo | blocks | fontsizeselect ' +
           'bold italic forecolor | alignleft aligncenter ' +
           'alignright alignjustify | bullist numlist outdent indent | ' +
           'searchreplace codesample code insertdatetime image emoticons link table ' +
-          'print help',
+          'print help lineheight',
         content_style: `body { font-family:Helvetica,Arial,sans-serif; font-size:15px } ` +
           `.mce-content-body[data-mce-placeholder]:not(.mce-visualblocks)::before { color: #9296b3; } ` +
-          `td { padding: 0 5px }`,
+          `td { padding: 0 5px } #tinymce { line-height: 0.4!important; }`,
         init_instance_callback: function (editor) {
           editor.addShortcut("ctrl+s", "Custom Ctrl+S", "custom_ctrl_s");
           editor.addCommand("custom_ctrl_s", function () {
